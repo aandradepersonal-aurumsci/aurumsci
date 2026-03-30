fd0ccea758541c46df11e96bad177146baf5cb55
Andre Luiz de Moura Andrade <andrepersonalnote@192.168.0.5>
2026-03-30 13:16:15 -0300

    fix: PostgreSQL suporte no database.py

--- app/database.py
+++ app/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
+is_sqlite = settings.DATABASE_URL.startswith("sqlite")
 engine = create_engine(
     settings.DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args={"check_same_thread": False} if is_sqlite else {}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
